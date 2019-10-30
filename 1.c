61a8fcd65ce0258a823a4966872e0d26bf49aa7d
kooyoonsung <56902075+embeddedmju01@users.noreply.github.com>
2019-10-30 19:31:12 +0900

    Add files via upload

---
+++ 1.c
@@ -0,0 +1,9 @@
+#include "myProject.h"
+
+int printMyName1(void)
+{
+	printf("my name is 구윤\n");
+
+	return 1;
+}
+성
--- myProject.h
+++ myProject.h
@@ -1,4 +1,4 @@
 
 
-
+int printMyName1(void);		//  kys
 int printMyName4(void);		// phj
