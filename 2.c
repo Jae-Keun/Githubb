d7b8f2f1acb00e1d6a72e6a10a1f898c2b9b0ab2
sincheongyong <56902503+sincheongyong@users.noreply.github.com>
2019-10-30 20:51:32 +0900

    Add files via upload

---
+++ 2.c
@@ -0,0 +1,8 @@
+#include "myProject.h"
+
+int printMyName2(void)
+{
+	printf("my name is 신청용\n");
+
+	return 1;
+}
--- myProject.h
+++ myProject.h
@@ -1,4 +1,5 @@
 
 
 int printMyName1(void);		//  kys
+int printMyName2(void);
 int printMyName4(void);		// phj
