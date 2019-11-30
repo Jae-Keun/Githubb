3d5cf1f0509f90122a62dda65b4ba0e157a7449f
TyoRan <zack96115@naver.com>
2019-11-30 16:06:57 +0900

    led, button, buzzer
    add led, button, buzzer

---
+++ homework/mylib.c
@@ -0,0 +1,65 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <sys/ioctl.h>
+#include <ctype.h>
+#include <sys/ipc.h>
+#include <sys/types.h>
+#include <sys/stat.h>
+#include <fcntl.h>
+#include <unistd.h>
+
+#include "led.h"
+#include "button.h"
+#include "buzzer.h"
+
+int main()
+{
+	int stat = 0;
+	int cnt = 0;
+	
+	ledLibInit();
+	buttonLibInit();
+	buzzerLibInit();
+	
+	ledAllOn();
+	sleep(1);
+	ledAllOff();
+	sleep(1);
+	
+	while(1)
+	{
+		if(stat==0)
+		{
+			ledOnOff(cnt,1);
+			buzzerLibOnBuz(440);
+			cnt += 1;
+			printf("%d\n", ledStatus());	
+			sleep(1);
+			if(cnt == 8)
+			{
+				stat = 1;
+				cnt -= 1;
+			}
+		}
+		else if(stat ==1)
+		{
+			ledOnOff(cnt,0);
+			buzzerLibOffBuz();
+			cnt -= 1;
+			printf("%d\n", ledStatus());	
+			sleep(1);
+			if(cnt == -1)
+			{
+				stat = 0;
+				cnt += 1;
+			}			
+		}
+	}
+
+	ledLibExit();
+	buttonLibExit();
+	buzzerLibExit();
+	
+	return 0;
+}
