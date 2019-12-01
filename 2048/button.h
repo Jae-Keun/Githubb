4f581d2c37ec9767aecdd5638c5faa057d550eee
TyoRan <zack96115@naver.com>
2019-12-01 15:41:16 +0900

    2048 v1
    use fnd, button
    fnd show score
    button : up, down, left, right

---
+++ 2048/button.h
@@ -0,0 +1,16 @@
+#ifndef _BUTTON_H_
+#define _BUTTON_H_
+
+#define BUTTON_DRIVER_NAME "/dev/input/event4"
+#define MESSAGE_ID 1122
+
+char button_input;
+
+typedef struct{
+	long int messageNum;
+	int keyInput;
+} BUTTON_MSG_T;
+
+int buttonLibInit(void);
+int buttonLibExit(void);
+#endif //_BUTTON_H_
