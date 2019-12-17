452226446a6cc6098c12b12a5473dabd5d820344
embeddedhcs <embeddedhcs@mju.ac.kr>
2019-12-18 05:49:46 +0900

    add header
    add header

---
+++ 2048/button.h
@@ -0,0 +1,21 @@
+#ifndef _BUTTON_H_
+#define _BUTTON_H_
+
+#define BUTTON_DRIVER_NAME "/dev/input/event5"
+#define MESSAGE_ID 1122
+
+#define UP 1
+#define DOWN 2
+#define LEFT 3
+#define RIGHT 4
+#define RESTART 5
+#define QUIT 6
+
+typedef struct{
+	long int messageNum;
+	int keyInput;
+} BUTTON_MSG_T;
+
+int buttonLibInit(void);
+int buttonLibExit(void);
+#endif //_BUTTON_H_
